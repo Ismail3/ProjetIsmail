a0dc9a57c804163b57b55705014e984297d2f28b
Ismail3 <ismail34alaoui@gmail.com>
2018-03-11 23:54:03 +0100

    Requêtes dans la base de données.

--- siteWeb_Apprendre@Comprendre/models/basesDeDonnées/03_Query.sql
+++ siteWeb_Apprendre@Comprendre/models/basesDeDonnées/03_Query.sql
@@ -18,3 +18,48 @@ mysql> show tables;
 11 rows in set (0,00 sec)
 */
 
+/*
+  Élèves
+
+mysql> desc Personne;
++------------------+--------------+------+-----+-------------------+----------------+
+| Field            | Type         | Null | Key | Default           | Extra          |
++------------------+--------------+------+-----+-------------------+----------------+
+| id               | int(11)      | NO   | PRI | NULL              | auto_increment |
+| nom              | varchar(255) | YES  |     | NULL              |                |
+| prenom           | varchar(255) | YES  |     | NULL              |                |
+| email            | varchar(255) | YES  |     | NULL              |                |
+| date_naissance   | date         | YES  |     | NULL              |                |
+| mot_de_passe     | varchar(255) | YES  |     | NULL              |                |
+| date_inscription | datetime     | YES  |     | CURRENT_TIMESTAMP |                |
++------------------+--------------+------+-----+-------------------+----------------+
+7 rows in set (0,00 sec)
+
+mysql> desc Eleve;
++--------------+---------+------+-----+---------+----------------+
+| Field        | Type    | Null | Key | Default | Extra          |
++--------------+---------+------+-----+---------+----------------+
+| id           | int(11) | NO   | PRI | NULL    | auto_increment |
+| id_personne  | int(11) | NO   | MUL | NULL    |                |
+| niveau_etude | int(11) | YES  | MUL | NULL    |                |
++--------------+---------+------+-----+---------+----------------+
+3 rows in set (0,00 sec)
+
+mysql> desc NiveauEtude;
++-------+--------------+------+-----+---------+----------------+
+| Field | Type         | Null | Key | Default | Extra          |
++-------+--------------+------+-----+---------+----------------+
+| id    | int(11)      | NO   | PRI | NULL    | auto_increment |
+| nom   | varchar(255) | YES  |     | NULL    |                |
++-------+--------------+------+-----+---------+----------------+
+2 rows in set (0,00 sec)
+ */
+
+ SELECT P.id as id,P.nom as nom,prenom,email,date_naissance,NE.nom as niveau_etude
+ FROM Eleve E, NiveauEtude NE, Personne P
+ WHERE E.id_personne = P.id and E.niveau_etude = NE.id
+ LIMIT 10
+ ;
+
+
+
