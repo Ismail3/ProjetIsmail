87466dbc05a8cfb73d7a3cde9a60cb8a2c558485
Ismail3 <ismail34alaoui@gmail.com>
2018-05-16 22:03:33 +0200

    Gestion des cours :
     Terminé :
    -Ajout d'un bouton sur les cours afin de les publier ou de les dépublier.
    
     À faire :
    -Modification d'un cours
    -Suppresion d'un cours
    -Inscription à un cours --> Gestion des séances de cours

--- siteWeb_Apprendre@Comprendre/controlleurs/pages/TableauDeBordControleur.php
+++ siteWeb_Apprendre@Comprendre/controlleurs/pages/TableauDeBordControleur.php
@@ -1623,9 +1623,32 @@ class TableauDeBordControleur extends ConnectedUserControleur
         echo '<div class="w3-container" style="padding:128px 16px">';
         $this->displayAddCoursEnseignant();
         echo '<br/>';
+        echo '<div class="container">
+  <h2>Dynamic Pills</h2>
+  <p>To make the tabs toggleable, add the data-toggle="pill" attribute to each link. Then add a .tab-pane class with a unique ID for every tab and wrap them inside a div element with class .tab-content.</p>
+  <ul class="nav nav-pills">
+    <li class="active"><a data-toggle="pill" href="#home">Home</a></li>
+    <li><a data-toggle="pill" href="#menu1">Menu 1</a></li>
+    <li><a data-toggle="pill" href="#menu2">Menu 2</a></li>
+  </ul>
+  
+  <div class="tab-content">';
+        echo'
+    <div id="home" class="tab-pane fade in active">';
         $this->displayListeCoursEnseignant();
-        echo '<br/>';
+        echo'</div>';
+        echo'
+    <div id="menu1" class="tab-pane fade">';
         $this->displayListeSeanceCoursEnseignant();
+        echo' </div>';
+        echo'
+    <div id="menu2" class="tab-pane fade">
+      <h3>Menu 2</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+  </div>
+</div>';
+        echo '<br/>';
         echo '</div>';
     }
 
--- siteWeb_Apprendre@Comprendre/templates/pages/tableauDeBord/tableauDeBordCours.php
+++ siteWeb_Apprendre@Comprendre/templates/pages/tableauDeBord/tableauDeBordCours.php
@@ -18,7 +18,10 @@ session_start();
     <link rel="stylesheet" href="../../../ressources/styles/bootstrap.min.css">
     <link rel="stylesheet" href="../../../ressources/styles/apprendre@comprendre.css">
     <link rel="stylesheet" href="../../../ressources/styles/template-tableau-de-bord.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script type="text/javascript" charset="UTF-8" src="../../../ressources/javascript/common.js"></script>
     <script type="text/javascript" charset="UTF-8" src="../../../ressources/javascript/map.js"></script>
     <script type="text/javascript" charset="UTF-8" src="../../../ressources/javascript/util.js"></script>
